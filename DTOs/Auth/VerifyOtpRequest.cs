4a1ce12eb4a9d6ff06ae1151c29965b7f15518e5
Diep Nguyen <nguyenquangdiepzed1@gmail.com>
2026-06-07 11:31:51 +0700

    feat: create OtpVerifyPassword.

---
+++ DTOs/Auth/VerifyOtpRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HeThongQuanLyThuVien.DTOs.Auth
+{
+    public class VerifyOtpRequest
+    {
+        [Required(ErrorMessage = "Email không được để trống")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "OTP không được để trống")]
+        public string Otp { get; set; } = string.Empty;
+    }
+}
---
+++ DTOs/BookCopies/CreateBookCopyResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace HeThongQuanLyThuVien.DTOs.BookCopies
+{
+    public class CreateBookCopyResponse
+    {
+        public int TotalCreated { get; set; }
+        public List<BookCopyResponse> Copies { get; set; } = new List<BookCopyResponse>();
+    }
+}
